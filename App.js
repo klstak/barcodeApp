76824af15545df3c1ef591b6f1e8ae8a11edcf46
klstak <michaelsimanjuntak564@gmail.com>
2022-10-16 18:42:59 +0700

    BarCode/QRCode ScannerApp

--- App.js
+++ App.js
@@ -1,11 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  const [text, setText] = useState('Belum melakukan scan')
+
+  const askForCameraPermission = () => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })()
+  }
+
+  // Meminta izin kamera
+  useEffect(() => {
+    askForCameraPermission();
+  }, []);
+
+  // Apa yang terjadi ketika memindai barcode
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    setText(data)
+    console.log('Type: ' + type + '\nData: ' + data)
+  };
+
+  // Memeriksa izin dan mengembalikan layar
+  if (hasPermission === null) {
+    return (
+      <View style={styles.container}>
+        <Text>Meminta izin kamera</Text>
+      </View>)
+  }
+  if (hasPermission === false) {
+    return (
+      <View style={styles.container}>
+        <Text style={{ margin: 10 }}>Tidak ada akses ke kamera</Text>
+        <Button title={'Allow Camera'} onPress={() => askForCameraPermission()} />
+      </View>)
+  }
+
+  // Tampilan
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.barcodebox}>
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+          style={{ height: 400, width: 400 }} />
+      </View>
+      <Text style={styles.maintext}>{text}</Text>
+
+      {scanned && <Button title={'Scan again?'} onPress={() => setScanned(false)} color='blue' />}
     </View>
   );
 }
@@ -17,4 +65,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  maintext: {
+    fontSize: 16,
+    margin: 20,
+  },
+  barcodebox: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 300,
+    width: 300,
+    overflow: 'hidden',
+    borderRadius: 30,
+    backgroundColor: 'blue',
+  }
 });
---
+++ readme.md
@@ -0,0 +1,12 @@
+###### Nama Aplikasi : Barcode Scanner
+
+###### Deskripsi Aplikasi : Barcode Scanner merupakan Aplikasi Scanner Barcode / QR Code yang dibuat dengan React Native dan Expo
+
+###### Daftar package yang digunakan (expo) :
+
+* Status Bar
+* BarCode Scanner
+
+###### Cara mengintall aplikasi (dalam mode pengembangan) :
+
+* jalankan **npm start** atau **npx expo start**
